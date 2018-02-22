376e6412e0fd8ad812692164920f0e9c9bd58470
DrewStock <andrewdstock@gmail.com>
2018-02-21 17:47:43 -0800

    removed route test components and app.js, added posts_index.js

--- src/index.js
+++ src/index.js
@@ -4,40 +4,15 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>Hello!</h2>
-            </div>
-        )
-    }
-}
-
-class Goodbye extends React.Component {
-    render() {
-        return (
-            <div>    
-                <h2>Goodbye!</h2>
-            </div>
-        )
-    }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
         <div>
-            <h1>Greeting app</h1>
-            <a href="/hello">Say hello</a>
-            <a href="goodbye">Say goodbye</a>            
-            <Route path="/hello" component={Hello}/>
-            <Route path="/goodbye" component={Goodbye}/>
+            
         </div>
     </BrowserRouter>
   </Provider>
